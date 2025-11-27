2ef20b729a3947495431287dccfd4d620cbe6530
KJY <dbvcxz@gmail.com>
2025-11-27 10:53:40 +0900

    version4

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -3,7 +3,7 @@ package chapter02;
 public class Branch01{
     
     public Branch01(){
-        System.out.println(x:"메인 브랜치에서 작업");
+        System.out.println(x:"충돌 테스트");
         System.out.println(x:"다음 브랜치에서 작업");
     }
 }
